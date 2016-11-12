d808ea2e5e123ff63b84e45652b39ac0f2763fc6
Kapunahele Wong <kapunahelewong@gmail.com>
2016-11-12 12:08:16 -0500

    added passwordMatcher()

--- app/reactive-forms/reactive-form.component.ts
+++ app/reactive-forms/reactive-form.component.ts
@@ -2,23 +2,30 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+function passwordMatcher(c: AbstractControl) {
+  return c.get('password').value === c.get('confirm').value
+    ? null : {'nomatch': true};
+}
+
 @Component({
   moduleId: module.id,
   selector: 'reactive-form',
   templateUrl: './reactive-form.component.html'
 })
 
-
 export class ReactiveFormComponent {
 
   form: FormGroup;
+
   constructor(private _fb: FormBuilder) {
     this.form = this._fb.group({
       first: '',
       last: '',
-      username: '',
-      password: ['', Validators.required],
-      confirm: ['', Validators.required],
+      account: this._fb.group({
+	      username: '',
+	      password: ['', Validators.required],
+	      confirm: ['', Validators.required]
+      }, {validator:passwordMatcher}),
       address: this._fb.group({
 	      street: '',
 	      city: '',
