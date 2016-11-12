a621c2841ad5352dfae550e2f7ef5e2291d6f4c0
Kapunahele Wong <kapunahelewong@gmail.com>
2016-11-12 13:57:47 -0500

    Add addresses with FormArray

--- app/reactive-forms/reactive-form.component.ts
+++ app/reactive-forms/reactive-form.component.ts
@@ -1,6 +1,6 @@
 
 import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 function passwordMatcher(c: AbstractControl) {
   return c.get('password').value === c.get('confirm').value
@@ -16,6 +16,7 @@ function passwordMatcher(c: AbstractControl) {
 export class ReactiveFormComponent {
 
   form: FormGroup;
+  addresses: FormArray;
 
   constructor(private _fb: FormBuilder) {
     this.form = this._fb.group({
@@ -26,13 +27,28 @@ export class ReactiveFormComponent {
 	      password: ['', Validators.required],
 	      confirm: ['', Validators.required]
       }, {validator:passwordMatcher}),
-      address: this._fb.group({
-	      street: '',
-	      city: '',
-	      state: '',
-	      zip: ''
-      })
+      addresses: this.buildArray()
     });
-  }
+   }
+
+   buildArray() : FormArray {
+   	this.addresses = this._fb.array([
+   		this.buildGroup()
+   		]);
+	    return this.addresses
+	}
+
+	buildGroup() : FormGroup {
+	    return this._fb.group({
+	        street: '',
+	        city: '',
+	        state: '',
+	        zip: ''
+    	});
+  	}
+
+  	add() {
+  		this.addresses.push(this.buildGroup());
+  	}
 }
 
